0edd8936198b4d4cf3731becb95288961df60caa
makkhawan-sardlan-350 <makkhawan.sardlan@intrachai.ac.th>
2026-05-21 14:27:10 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
 	"net/http"
 	"os/exec"
 )
@@ -14,15 +11,15 @@ func shutdown(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
+	// conn, err := net.Dial("udp", "8.8.8.8:80")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer conn.Close()
 
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	// localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-	fmt.Println("Local IP:", localAddr.IP)
+	// fmt.Println("Local IP:", localAddr.IP)
 
 	http.HandleFunc("/shutdown", shutdown)
 	http.ListenAndServe(":12500", nil)
